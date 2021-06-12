bfcfcd5b1c636576782f9765da9eee277b265068
miketwheeler <mikeytwheeler@gmail.com>
2021-06-11 22:48:07 -0700

    static props

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import path from 'path';
+import fs from 'fs/promises';
+import Link from 'next/link';
 
 function HomePage(props) {
 	const { products } = props;
 	return (
 		<ul>
 			{products.map((product) => (
-				<li key={product.id}>{product.title}</li>
+				<li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
 			))}
 		</ul>
 	);
 }
 
-export async function getStaticProps(){
+export async function getStaticProps(context){
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+	
+	// if (!data){
+	// 	return {
+	// 		redirect: {
+	// 			destination:'/no-data',
+	// 		}
+	// 	}
+	// }
 	return {
 		props: {
-			products: [{ id: 'p1', title: 'product 1' }],
+			products: data.products,
 		},
+		revalidate: 10,
 	};
 }
 
